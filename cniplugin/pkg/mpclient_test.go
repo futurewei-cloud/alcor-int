39ffb9314dd45605885b1404d6d7235c0f9b803f
Hong Chen <hong.chen@futurewei.com>
2019-12-17 10:59:33 -0800

    trimmed off double quotes around mac & status; added table-driven test cases for port info parser

--- cniplugin/pkg/mpclient_test.go
+++ cniplugin/pkg/mpclient_test.go
@@ -4,21 +4,84 @@ import "testing"
 
 func TestParseGetPortResp(t *testing.T) {
 	subnetID := "a87e0f87-a2d9-44ef-9194-9a62f178594e"
-	body := `
+
+	tcs := []struct{
+		desc string
+		responseBody string
+		expectedIP, expectedMAC, expectedStatus string
+		expectingError bool
+	} {
+		{
+			desc: "single record",
+			responseBody: `
 {
-  "portId":"1111111", 
-  "status":"UP", 
-  "macAddress":"02:42:32:43:60:bf", 
+  "portId":"1111111",
+  "status":"UP",
+  "macAddress":"02:42:32:43:60:bf",
   "fixedIps":[
-    {"subnetId":"a87e0f87-a2d9-44ef-9194-9a62f178594e", "ipAddress":"123.45.67.8/24"}
+    {"subnetId":"----", "ipAddress":"123.45.67.8/24"}
   ]
 }
-`
-
-	port, err := parseGetPortResp(subnetID, []byte(body))
-	if err != nil {
-		t.Errorf("unexpected error: %v", err)
+`,			expectedIP: "123.45.67.8/24",
+			expectedMAC: "02:42:32:43:60:bf",
+			expectedStatus: "UP",
+		},
+		{
+			desc: "multi records",
+			responseBody: `
+{
+  "portId":"1111111",
+  "status":"WIP",
+  "macAddress":"00:11:22:33:44:55",
+  "fixedIps":[
+    {"subnetId":"11111111-1111-1111-1111-111111111111", "ipAddress":"1.1.1.1/24"},
+    {"subnetId":"a87e0f87-a2d9-44ef-9194-9a62f178594e", "ipAddress":"2.2.2.2/16"}
+  ]
+}
+`,			expectedIP: "2.2.2.2/16",
+			expectedMAC: "00:11:22:33:44:55",
+			expectedStatus: "WIP",
+		},
+		{
+			desc: "no ip record",
+			responseBody: `
+{
+  "portId":"1111111",
+  "status":"UP",
+  "macAddress":"02:42:32:43:60:bf",
+  "fixedIps":[]
+}
+`,			expectingError: true,
+		},
 	}
 
-	t.Logf("port detail: %v", port)
+	for _, tc := range tcs {
+		info, err := parseGetPortResp(subnetID, []byte(tc.responseBody))
+
+		if !tc.expectingError {
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+			}
+
+			if err == nil {
+				if tc.expectedIP != info.IP {
+					t.Errorf("%q: expected %v, got %v", tc.desc, tc.expectedIP, info.IP)
+				}
+				if tc.expectedMAC != info.MAC {
+					t.Errorf("%q: expected %q, got %q", tc.desc, tc.expectedMAC, info.MAC)
+				}
+				if tc.expectedStatus != string(info.Status) {
+					t.Errorf("%q: expected %q, got %q", tc.desc, tc.expectedStatus, info.Status)
+				}
+			}
+		}
+
+		if tc.expectingError {
+			if err == nil {
+				t.Errorf("%q: expected error; got nil", tc.desc)
+			} else {
+				t.Logf("%q: expected error; got %v", tc.desc, err)
+			}
+		}
+	}
 }
