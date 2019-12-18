5e26616555f5a2a2a3b6dd81e17fca118259073a
Hong Chen <hong.chen@futurewei.com>
2019-12-17 17:36:19 -0800

    restructed file/folder structures

--- cniplugin/pkg/nic.go
+++ cniplugin/pkg/nic.go
@@ -5,6 +5,7 @@ import (
 	"github.com/vishvananda/netlink"
 )
 
+// FindNicInNs tries to probe nic inside netns
 func FindNicInNs(device, nsPath string) error {
 	return ns.WithNetNSPath(nsPath, func(nsOrig ns.NetNS) error {
 		_, err := netlink.LinkByName(device)
---
+++ cniplugin/pkg/subnet.go
@@ -0,0 +1,47 @@
+package pkg
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/go-resty/resty/v2"
+	"net"
+	"net/http"
+	"path"
+)
+
+// subnet is struct to facilitate json parsing
+type subnet struct {
+	GatewayIP string `json:"gatewayIp"`
+	CIDR string `json:"cidr"`
+}
+
+func parseSubnetInfo(data []byte) (*Subnet, error) {
+	subnet := &subnet{}
+	if err := json.Unmarshal(data, subnet); err != nil {
+		return nil, err
+	}
+
+	gwip := net.ParseIP(subnet.GatewayIP)
+	_, netmask, err := net.ParseCIDR(subnet.CIDR)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Subnet{Gateway: gwip, Netmask: netmask.Mask}, nil
+}
+
+func (m client) GetSubnet(projectID, subnetID string) (*Subnet, error) {
+	url := *m.url
+	url.Path = path.Join(url.Path, "project", projectID, "subnet", subnetID)
+	resp, err := resty.New().R().Get(url.String())
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode() != http.StatusOK {
+		return nil, fmt.Errorf("failed, method=GET, url=%s, status cod=%d", m.url.String(), resp.StatusCode())
+	}
+
+	return parseSubnetInfo(resp.Body())
+}
+
