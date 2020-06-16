7c06ce3609158928af3e1e0f6a00e87fd8c90434
lly00 <lly00@mail.ustc.edu.cn>
2020-06-16 12:17:32 +0800

    Update ConsumerRunnable.java

--- tools/rocketmq-test/client/src/main/java/demo/ConsumerRunnable.java
+++ tools/rocketmq-test/client/src/main/java/demo/ConsumerRunnable.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
-import org.apache.rocketmq.client.consumer.listener. ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
